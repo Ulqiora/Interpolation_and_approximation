025d02804a17779c3ec811fcbd4602f2593ff0f9
Jcraster <jcraster@student.21-school.ru>
2023-03-28 15:05:49 +0700

    Finish delete bags

--- src/Model/Interpolation/NewtonPolynomInterp/NewtonPolynomInterp.h
+++ src/Model/Interpolation/NewtonPolynomInterp/NewtonPolynomInterp.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
 #include "../IInterpolation2D.h"
 
-class NewtonPolynomInterp : public IInterpolation{
- public:
+class NewtonPolynomInterp : public IInterpolation {
+   public:
     using SubTable = std::vector<std::vector<double>>;
-    // [[nodiscard]]
-    virtual IGraphByDiscrete2D* CreateFunction(const std::vector<double>& X,const std::vector<double>& Y);
- private:
-    std::vector<double> CreateColOfSub(const std::vector<double>& col,size_t i);
+    [[nodiscard]] virtual IGraphByDiscrete2D* CreateFunction(
+        const std::vector<double>& X, const std::vector<double>& Y);
+
+   private:
+    std::vector<double> CreateColOfSub(const std::vector<double>& col,
+                                       size_t i);
 };
--- src/main.cc
+++ src/main.cc
@@ -7,11 +7,11 @@
 #include "Model/Interpolation/NewtonPolynomInterp/NewtonPolynomInterp.h"
 
 int main(){
-    IGraphing2DByDiscrete* graphing=new NewtonPolynomInterp();
-    std::vector<double> X{1, 3.5, 6, 8.5, 11, 13.5, 16};
-    std::vector<double> Y{1.4142,3.3741,4.4375,5.2223,5.862,6.4106,6.8957};
+    IGraphing2DByDiscrete* graphing=new CubicSplines();
+    std::vector<double> X{1,2,4,7};
+    std::vector<double> Y{2,3,1,4};
     std::cout<<X.size()<<"  "<<Y.size()<<'\n';
     auto temp=graphing->CreateFunction(X,Y);
-    std::cout<<temp->calc(8.5);
+    std::cout<<temp->calc(7);
     return 0;
 }
