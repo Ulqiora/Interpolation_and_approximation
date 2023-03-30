a0a204685e7c17bfe2087637183fb4c7d9eacb90
Ulqiora <damdinovandrey@gmail.com>
2023-03-30 13:40:51 +0700

    edit worning

--- src/Model/Interpolation/CubicSplines/CubicSplines.cc
+++ src/Model/Interpolation/CubicSplines/CubicSplines.cc
@@ -6,12 +6,12 @@ IGraphByDiscrete2D *CubicSplines::CreateFunction(const std::vector<double> &X,
     size_t size_=X.size()-1;
     Matrix coeffs(size_ - 1, size_);
     std::vector<std::vector<double>> res(4,std::vector<double>(size_));
-    std::vector<double> h(size_ - 1);
+    std::vector<double> h(size_);
     for (size_t i = 0; i < size_; i++) {
         h[i]=X[i + 1] - X[i];
-        std::cout<<h[i]<<' ';
+        // std::cout<<h[i]<<' ';
     }
-    std::cout<<'\n';
+    // std::cout<<'\n';
     for (size_t i = 1, j = 0; i < size_; i++, j++) {
         if (i > 1) coeffs(j, j - 1) = 1.0 / 3.0 * h[i - 1];
         coeffs(j, j) = 2.0 / 3.0 * (h[i - 1] + h[i]);
@@ -19,10 +19,10 @@ IGraphByDiscrete2D *CubicSplines::CreateFunction(const std::vector<double> &X,
         coeffs(j, size_-1) =(Y[i + 1] - Y[i]) / h[i] -
                             (Y[i] - Y[i - 1]) / h[i-1];
     }
-    coeffs.print(std::cout);
+    // coeffs.print(std::cout);
     res[C] = slea.start(coeffs).answer;
     res[C].insert(res[C].begin(), 0.0);
-    std::cout<<"KU"<<res[A].size()<<"  "<<res[B].size()<<"  "<<res[C].size()<<"  "<<res[D].size()<<'\n';
+    // std::cout<<"KU"<<res[A].size()<<"  "<<res[B].size()<<"  "<<res[C].size()<<"  "<<res[D].size()<<'\n';
     for (unsigned i = 0; i < size_; i++) {
         res[A][i] = Y[i];
         res[B][i] = (Y[i+1] - Y[i]) / h[i] -
